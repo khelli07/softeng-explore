66a884bf2db95d0ddcf52611d5ce574ffd40fd64
Maria Khelli <13520115@std.stei.itb.ac.id>
2023-03-19 19:06:19 +0700

    webgl - hollow pyramid

--- webgl/3d/app.js
+++ webgl/3d/app.js
@@ -65,7 +65,7 @@ const main = function () {
 
     // Specify cull face so the program does not draw back
 	gl.enable(gl.CULL_FACE);
-	gl.frontFace(gl.CW);
+	gl.frontFace(gl.CCW);
 	gl.cullFace(gl.BACK);
 
     // Initialize shaders and program
@@ -75,45 +75,141 @@ const main = function () {
     gl.useProgram(program);
 
     // Define vertex and indices
-    let {r, g, b} = {r: 0.5, g: 0.5, b: 0.5};
+    let {r, g, b} = {r: 0, g: 0, b: 0};
     const vertices = [
         // x, y, z,     r, g, b
-
-        // Front face
-        0.0, 0.75, 0.0,     r*1.5, g/2, b/2, // top point
-        0.5, -0.5, 0.5,     r*1.5, g/2, b/2,
-        -0.5, -0.5, 0.5,    r*1.5, g/2, b/2,
-
-        // Back face
-        0.0, 0.75, 0.0,     r, g/2, b/2, // top point
-        -0.5, -0.5, -0.5,    r, g/2, b/2,
-        0.5, -0.5, -0.5,     r, g/2, b/2,
-
-        // Left face
-        0.0, 0.75, 0.0,     r/2, g*1.5, b/2, // top point
-        0.5, -0.5, -0.5,     r/2, g*1.5, b/2,
-        0.5, -0.5, 0.5,    r/2, g*1.5, b/2,
+        0, 0.4, 0,                r+0.75, g+0.75, b+0.75,
         
-        // Right face
-        0.0, 0.75, 0.0,     r/2, g/2, b*1.5, // top point
-        -0.5, -0.5, 0.5,    r/2, g/2, b*1.5,
-        -0.5, -0.5, -0.5,     r/2, g/2, b*1.5,
-
-        // Base
-        0.5, -0.5, 0.5,     0.85, 0.65, 0,
-        0.5, -0.5, -0.5,     0.85, 0.65, 0,
-        -0.5, -0.5, 0.5,     0.85, 0.65, 0,
-        -0.5, -0.5, -0.5,     0.85, 0.65, 0,
+        // Top square
+        -0.075, 0.5, 0.0,           r, g, b,
+        0.0, 0.5, -0.075,           r, g, b,
+        0.075, 0.5, 0.0,            r, g, b,
+        0.0, 0.5, 0.075,            r, g, b,
+        
+        // Right-back square
+        0.4, -0.5, -0.5,        r, g, b,
+        0.5, -0.5, -0.5,        r, g, b,
+        0.5, -0.5, -0.4,        r, g, b,
+        0.4, -0.4, -0.35,        r+0.75, g+0.75, b+0.75,
+        
+        // Left-back square
+        -0.5, -0.5, -0.5,       r, g, b,
+        -0.4, -0.5, -0.5,       r, g, b,
+        -0.4, -0.4, -0.35,       r+0.75, g+0.75, b+0.75,
+        -0.5, -0.5, -0.4,       r, g, b,
+        
+        // Right-front square
+        0.4, -0.4, 0.35,        r+0.75, g+0.75, b+0.75,
+        0.5, -0.5, 0.4,        r, g, b,
+        0.5, -0.5, 0.5,        r, g, b,
+        0.4, -0.5, 0.5,        r, g, b,
+        
+        // Left-front square
+        -0.5, -0.5, 0.4,       r, g, b,
+        -0.4, -0.4, 0.35,       r+0.75, g+0.75, b+0.75,
+        -0.4, -0.5, 0.5,       r, g, b,
+        -0.5, -0.5, 0.5,       r, g, b, 
     ];
 
     const indices = [
-        0, 1, 2,
-        3, 4, 5,
-        6, 7, 8,
-        9, 10, 11,
+        // Top close
+        1, 2, 3,
+        3, 4, 1,
 
-        12, 13, 14, 
-        15, 14, 13
+        // Top to right back
+        0, 2, 3,
+        8, 7, 5,
+
+        2, 5, 7,
+        7, 3, 2,
+        
+        5, 2, 0,
+        0, 8, 5,
+
+        3, 7, 8,
+        8, 0, 3,
+
+        // Top to left back
+        0, 1, 2,
+        11, 10, 12,
+
+        10, 2, 1,
+        1, 12, 10,
+
+        11, 0, 2,
+        2, 10, 11,
+
+        12, 1, 0,
+        0, 11, 12,
+        
+        // Top to right front
+        0, 3, 4,
+        13, 16, 14,
+
+        4, 3, 14,
+        14, 16, 4,
+
+        0, 13, 14,
+        14, 3, 0,
+
+        0, 4, 16,
+        16, 13, 0,
+        
+        // Top to left front
+        0, 4, 1,
+        18, 17, 19,
+
+        17, 1, 4,
+        4, 19, 17,
+
+        0, 1, 17, 
+        17, 18, 0,
+
+        0, 18, 19,
+        19, 4, 0,
+
+        // Left back to right back
+        5, 10, 12,
+        12, 7, 5,
+
+        10, 5, 8,
+        8, 11, 10,
+
+        11, 8, 7,
+        7, 12, 11,
+
+        // Right front to left front
+        14, 17, 19, 
+        19, 16, 14,
+
+        18, 13, 16,
+        16, 19, 18,
+        
+        17, 14, 13, 
+        13, 18, 17,
+
+        // Right back to right front
+        7, 5, 16,
+        16, 14, 7,
+
+        7, 14, 13,
+        13, 8, 7,
+
+        13, 16, 5,
+        5, 8, 13,
+
+        // Left back to left front
+        10, 12, 17,
+        17, 19, 10,
+
+        12, 11, 18,
+        18, 17, 12,
+
+        11, 18, 19,
+        19, 10, 11,
+
+        18, 11, 10,
+        10, 19, 18,            
     ];
 
     // Create buffer
@@ -152,7 +248,7 @@ const main = function () {
 
     // Define uniform matrix
     const translation = [0, 0, 0];
-    let rotation = [degToRad(0), degToRad(150), degToRad(20)];
+    let rotation = [degToRad(0), degToRad(0), degToRad(0)];
     const scale = [1, 1, 1];
 
     let matrix = getTransformationMatrix(translation, rotation, scale);
@@ -169,7 +265,7 @@ const main = function () {
     const loop = function () {
 		angle = performance.now() / 1000 / 6 * 2 * Math.PI;
 
-		rotation = [angle, 0, angle];
+		rotation = [degToRad(-20), angle, 0];
         matrix = getTransformationMatrix(translation, rotation, scale);
         gl.uniformMatrix4fv(matrixLocation, false, matrix);
 
